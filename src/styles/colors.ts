2607bffc6f8eb5df8f21ae2823b4aa5b73132aa7
lucasrmts2 <lucasrmts2@gmail.com>
2021-10-25 12:59:11 -0300

    feat: add aplication colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  green: 'rgb(61, 177, 127)',
+  blue: 'rgba(84, 182, 165, 1)',
+  gradient: '180deg, rgba(61, 177, 127, 1) 0%, rgba(84, 182, 165, 1) 100%',
+  graphite: '#333',
+  black: '#222222',
+  shadow: 'rgba(0, 0, 0, 0.6)',
+};
+
+export default colors;
