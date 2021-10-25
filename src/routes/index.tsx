9df1b4f00fd6518e3e1cdec1bc1910a1d1856743
lucasrmts2 <lucasrmts2@gmail.com>
2021-10-25 11:11:11 -0300

    feat: add home page route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import { FirstPage, SecondPage } from '../pages';
+import { HomePage, SecondPage } from '../pages';
 
 const Routes: FC = () => (
   <Switch>
-    <Route path="/" exact component={FirstPage} />
+    <Route path="/" exact component={HomePage} />
     <Route path="/second" component={SecondPage} />
   </Switch>
 );
